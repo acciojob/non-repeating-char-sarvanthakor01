7911330ac4c515892bc0c13a68e910f715ad4588
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-06 13:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(!str){
+		return null;
+	}
+	let map = new Map();
+	 for (let char of str) {
+        charMap.set(char, (charMap.get(char) || 0) + 1);
+    }
+
+    for (let [key, value] of charMap) {
+        if (value === 1) return key;
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+console.log(firstNonRepeatedChar("aabbcdd")); 
+console.log(firstNonRepeatedChar("aabbcc")); 
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
